aa2f79a55aeeebeccedaf2a32cf2c4612b6988d8
Lucia Pal <halucinka@gmail.com>
2016-04-18 21:56:56 -0400

    Add .gitignore.

--- linkedListFunctions.cc
+++ linkedListFunctions.cc
@@ -4,37 +4,23 @@ using namespace std;
 
 class Node{
 private:
+
+public:
     int data;
     Node* next;
-public:
-    Node(int data, Node* next){
-      this->data = data;
-      this->next = next;
+    Node(int data, Node* next) : this->data(data), this->next(next) {
     }
-    int getData(){
-      return data;
-    }
-    Node* getNext(){
-      return next;
-    }
-    void setData(int data){
-      this->data = data;
-    }
-    void setNext(Node* next){
-      this->next = next;
-    }
+
 };
 
 class LinkedList{
 private:
     Node* root;
 public:
-    LinkedList() {
-      root = nullptr;
+    LinkedList() : root(nullptr) {
     }
 
-    LinkedList(Node* root) {
-      root = root;
+    LinkedList(Node* root) : this->root(root) {
     }
 
     void setRoot(Node* root){
@@ -48,9 +34,9 @@ public:
       root = v;
     }
     int remove(){
-      int d = root->getData();
+      int d = root->data;
       Node* old_root = root;
-      root = root->getNext();
+      root = root->next;
       delete old_root;
       return d;
     }
@@ -58,8 +44,8 @@ public:
     void print(){
       Node* i = root;
       while (i != nullptr) {
-        cout << i->getData() << endl;
-        i = i->getNext();
+        cout << i->data << endl;
+        i = i->next;
       }
       cout <<endl;
     }
@@ -71,94 +57,95 @@ public:
     }
 
     void reverse(){
+      if (root != null) {
+        return;
+      }
       Node* i = root;
-      Node* temp = root;
-      while (i != nullptr) {
-        temp = i;
-        Node* j = i->getNext();
-        i->setNext(i);
+      Node* reverse_root = nullptr;
+      while (i->next != nullptr) {
+        Node* j = i->next;
+        i->next = reverse_root;
+        reverse_root = i;
         i = j;
       }
-      root->setNext(nullptr);
-      root = temp;
-      cout <<endl;
+      root = reverse_root;
     }
 
-    bool cycle(){
-      Node* a = root;
-      Node* b = root;
-      while ((a != b) & (a != nullptr) & (b != nullptr)) {
-        a = a->getNext();
-        b = (b->getNext())->getNext;
-    }
-    if ((a == nullptr) | (b == nullptr)) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  int len(){
-    int N = 0;
-    Node* i = root;
-    while (i != nullptr) {
-      N++;
-      i = i->getNext();
-    }
-    return N;
-  }
+  //   bool cycle(){
+  //     Node* a = root;
+  //     Node* b = root;
+  //     while ((a != b) & (a != nullptr) & (b != nullptr)) {
+  //       a = a->getNext();
+  //       b = (b->getNext())->getNext;
+  //   }
+  //   if ((a == nullptr) | (b == nullptr)) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+  //
+  // int len(){
+  //   int N = 0;
+  //   Node* i = root;
+  //   while (i != nullptr) {
+  //     N++;
+  //     i = i->getNext();
+  //   }
+  //   return N;
+  // }
 
 };
 
-
-class Stack zdedena z LinkedList{
-
-private:
-  maxlist LinkedList;
-public:
-    Stack() {
-      root = nullptr;
-      maxlist = ...?;
-    }
-  void push(int data) {
-    self.addFront(int data)
-    maxlist.addFront(max(maxlist.root->getData(), data))
-  }
-
-  void pop() {
-    self.setRoot(root->getNext())
-    maxlist.root.setRoot(root->getNext())
-    //vymazat vrcholy?
-  }
-
-  int top() {
-    return root->getData()
-  }
-
-  int max() {
-    return maxlist.root->getData()
-  }
-
-void
-
-void quickSort(LinkedList list) {
-   int N = list.len();
-   srand (time(NULL));
-   pivot = rand() % N + 1;
-   root1 = root;
-   root2 = pivot->getNext();
-   pivot->setNext(nullptr);
-   list1 = new LinkedList(root1);
-   list2 = new LinkedList(root2);
-   quickSort(list1);
-   quickSort(list2);
-   Node* i = list1.root1;
-   while (i->getNext() != nullptr) {
-     i = i->getNext();
-   }
-   i->setNext(root2);
-   list.setRoot(root1);
-}
+//
+// class Stack zdedena z LinkedList{
+//
+// private:
+//   maxlist LinkedList;
+// public:
+//     Stack() {
+//       root = nullptr;
+//       maxlist = ...?;
+//     }
+//   void push(int data) {
+//     self.addFront(int data)
+//     maxlist.addFront(max(maxlist.root->getData(), data))
+//   }
+//
+//   void pop() {
+//     self.setRoot(root->getNext())
+//     maxlist.root.setRoot(root->getNext())
+//     //vymazat vrcholy?
+//   }
+//
+//   int top() {
+//     return root->getData()
+//   }
+//
+//   int max() {
+//     return maxlist.root->getData()
+//   }
+//
+// void
+//
+// void quickSort(LinkedList list) {
+//    int N = list.len();
+//    srand (time(NULL));
+//    pivot = rand() % N + 1;
+//    root1 = root;
+//    root2 = pivot->getNext();
+//    pivot->setNext(nullptr);
+//    list1 = new LinkedList(root1);
+//    list2 = new LinkedList(root2);
+//    quickSort(list1);
+//    quickSort(list2);
+//    Node* i = list1.root1;
+//    while (i->getNext() != nullptr) {
+//      i = i->getNext();
+//    }
+//    i->setNext(root2);
+//    list.setRoot(root1);
+// }
 
 int main(){
     cout << "Hello World!" << endl;
@@ -171,7 +158,7 @@ int main(){
     list.print();
     list.addFront(3);
     list.print();
-    //list.reverse();
+    list.reverse();
     //cout << list.cycle();
     list.print();
     return 0;
