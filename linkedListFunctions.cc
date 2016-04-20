72432cf588ac8ba07d7f672cb77ba0e3fe41623d
Lucia Pal <halucinka@gmail.com>
2016-04-19 20:00:25 -0400

    Add quicsort and mergesort.

--- linkedListFunctions.cc
+++ linkedListFunctions.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <algorithm>
 using namespace std;
 
 class Node{
@@ -8,7 +9,7 @@ private:
 public:
     int data;
     Node* next;
-    Node(int data, Node* next) : this->data(data), this->next(next) {
+    Node(int data, Node* next) : data(data), next(next) {
     }
 
 };
@@ -20,13 +21,15 @@ public:
     LinkedList() : root(nullptr) {
     }
 
-    LinkedList(Node* root) : this->root(root) {
+    LinkedList(Node* root) : root(root) {
     }
 
-    void setRoot(Node* root){
-      this->root = root;
+
+    Node* getRoot(){
+      return root;
     }
-    bool empty(){
+
+    bool empty() const {
       return root == nullptr;
     }
     void addFront(int data){
@@ -41,13 +44,8 @@ public:
       return d;
     }
 
-    void print(){
-      Node* i = root;
-      while (i != nullptr) {
-        cout << i->data << endl;
-        i = i->next;
-      }
-      cout <<endl;
+    void print() const{
+      print(root);
     }
 
     ~LinkedList(){
@@ -57,12 +55,12 @@ public:
     }
 
     void reverse(){
-      if (root != null) {
+      if (root == nullptr) {
         return;
       }
       Node* i = root;
       Node* reverse_root = nullptr;
-      while (i->next != nullptr) {
+      while (i != nullptr) {
         Node* j = i->next;
         i->next = reverse_root;
         reverse_root = i;
@@ -71,95 +69,203 @@ public:
       root = reverse_root;
     }
 
-  //   bool cycle(){
-  //     Node* a = root;
-  //     Node* b = root;
-  //     while ((a != b) & (a != nullptr) & (b != nullptr)) {
-  //       a = a->getNext();
-  //       b = (b->getNext())->getNext;
-  //   }
-  //   if ((a == nullptr) | (b == nullptr)) {
-  //     return false;
-  //   } else {
-  //     return true;
-  //   }
-  // }
-  //
-  // int len(){
-  //   int N = 0;
-  //   Node* i = root;
-  //   while (i != nullptr) {
-  //     N++;
-  //     i = i->getNext();
-  //   }
-  //   return N;
-  // }
+    bool cycle() const{
+      if (root == nullptr) {
+        return false;
+      }
+      Node* a = root;
+      Node* b = root->next;
+      while ((a != b) && (a != nullptr) && (b != nullptr) && (b->next != nullptr)) {
+        a = a->next;
+        b = b->next->next;
+    }
+    if ((a == nullptr) || (b == nullptr) || (b->next == nullptr)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
+  int len() const {
+    int N = 0;
+    Node* i = root;
+    while (i != nullptr) {
+      N++;
+      i = i->next;
+    }
+    return N;
+  }
+
+  void quickSort() {
+    root = quickSort(this->root, this->len());
+  }
+
+  static Node* quickSort(Node* root, int N) {
+     if (N <= 1) {
+       return root;
+     }
+     srand(time(NULL));
+     const int num_pivot = rand() % N;
+     Node* pivot = root;
+     for (int i = 0; i < num_pivot; i++) {
+       pivot = pivot->next;
+     }
+     const int pivot_data = pivot->data;
+     Node* i = root;
+     Node* root1 = nullptr;
+     Node* root2 = nullptr;
+     int N1 = 0;
+     int N2 = 0;
+     while (i != nullptr) {
+       Node* j = i->next;
+       if (i->data < pivot_data){
+         N1++;
+         i->next = root1;
+         root1 = i;
+       } else {
+         N2++;
+         i->next = root2;
+         root2 = i;
+       }
+       i = j;
+     }
+     root1 = quickSort(root1, N1);
+     root2 = quickSort(root2, N2);
+     if (root1 == nullptr) {
+       return root2;
+     }
+     if (root2 == nullptr) {
+       return root1;
+     }
+     Node* k = root1;
+     while (k->next != nullptr) {
+       k = k->next;
+     }
+     k->next = root2;
+     return root1;
+  }
+
+  static void print(Node* root){
+    //cout << "print" << endl;
+    Node* i = root;
+    while (i != nullptr) {
+      cout << i->data << endl;
+      i = i->next;
+    }
+    //cout <<"-----------"<< endl;
+  }
+
+
+  void mergeSort() {
+    root = mergeSort(this->root, this->len());
+  }
+
+  static Node* mergeSort(Node* root, int N) {
+    //cout << "mergesort " << N << endl;
+    if (N <= 1) {
+      return root;
+    }
+    int num_pivot = (N-1)/2;
+    Node* pivot = root;
+    for (int i = 0; i < num_pivot; i++) {
+      pivot = pivot->next;
+    }
+    Node* root1 = root;
+    Node* root2 = pivot->next;
+    pivot->next = nullptr;
+    int N1 = num_pivot + 1;
+    int N2 = N - num_pivot - 1;
+    root1 = mergeSort(root1, N1);
+    root2 = mergeSort(root2, N2);
+    //cout << "po zotriedeni polovic " << N1 << " " << N2 << endl;
+    //cout << "root1" << endl;
+    //cout << "root2" <<endl;
+
+    if (root1->data < root2->data) {
+      root = root1;
+      root1 = root1->next;
+    } else {
+      root = root2;
+      root2 = root2->next;
+    }
+    Node* temp = root;
+    //cout << "pred while " << N1 << " " << N2 << endl;
+    while ((root1 != nullptr) && (root2 != nullptr)) {
+      //cout << "vo while " << N1 << " " << N2 << endl;
+      if (root1->data < root2->data) {
+        temp->next = root1;
+        root1 = root1->next;
+      } else {
+        temp->next = root2;
+        root2 = root2->next;
+      }
+      temp = temp->next;
+    }
+    if (root1 != nullptr) {
+      temp->next = root1;
+    } else {
+      temp->next = root2;
+    }
+    //cout << "printujem zotriedene "<< N1 << " " << N2 << endl;
+    return root;
+  }
 };
 
-//
-// class Stack zdedena z LinkedList{
-//
-// private:
-//   maxlist LinkedList;
-// public:
-//     Stack() {
-//       root = nullptr;
-//       maxlist = ...?;
-//     }
-//   void push(int data) {
-//     self.addFront(int data)
-//     maxlist.addFront(max(maxlist.root->getData(), data))
-//   }
-//
-//   void pop() {
-//     self.setRoot(root->getNext())
-//     maxlist.root.setRoot(root->getNext())
-//     //vymazat vrcholy?
-//   }
-//
-//   int top() {
-//     return root->getData()
-//   }
-//
-//   int max() {
-//     return maxlist.root->getData()
-//   }
-//
-// void
-//
-// void quickSort(LinkedList list) {
-//    int N = list.len();
-//    srand (time(NULL));
-//    pivot = rand() % N + 1;
-//    root1 = root;
-//    root2 = pivot->getNext();
-//    pivot->setNext(nullptr);
-//    list1 = new LinkedList(root1);
-//    list2 = new LinkedList(root2);
-//    quickSort(list1);
-//    quickSort(list2);
-//    Node* i = list1.root1;
-//    while (i->getNext() != nullptr) {
-//      i = i->getNext();
-//    }
-//    i->setNext(root2);
-//    list.setRoot(root1);
-// }
+
+class Stack {
+
+private:
+    LinkedList stack;
+    LinkedList maxlist;
+public:
+    Stack() : stack(), maxlist() {
+    }
+
+    void push(int data) {
+      stack.addFront(data);
+      if (maxlist.getRoot() == nullptr) {
+          maxlist.addFront(data);
+      } else {
+        maxlist.addFront(std::max(maxlist.getRoot()->data, data));
+      }
+    }
+
+    int pop() {
+      maxlist.remove();
+      return stack.remove();
+    }
+
+    int top() {
+      return stack.getRoot()->data;
+    }
+
+    int max() {
+      return maxlist.getRoot()->data;
+    }
+};
+
 
 int main(){
     cout << "Hello World!" << endl;
     LinkedList list;
     list.addFront(1);
-    list.print();
     list.addFront(2);
-    list.print();
-    list.remove();
-    list.print();
     list.addFront(3);
     list.print();
-    list.reverse();
-    //cout << list.cycle();
+    cout << "Dlzka: " << list.len() << endl;
+    cout << "sortime: " << endl;
+    list.mergeSort();
     list.print();
+    // Stack s;
+    // s.push(1);
+    // s.push(47);
+    // s.push(2);
+    // s.pop();
+    // //cout <<"top " << s.top() << endl;
+    // //cout <<"max " << s.max() << endl;
+
+
+    ////cout << "print reverse:" << endl;
+    //list.print();
     return 0;
 }
